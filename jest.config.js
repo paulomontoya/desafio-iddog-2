5e7bdf45af920d592831ebb02895857a712770e0
Paulo Montoya <paulo@paulomontoya.com.br>
2019-07-11 02:29:41 -0300

    Jest: Map scss files with identity-obj-proxy

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-    "^.+\\.scss$": "jest-transform-css"
-  }
-};
