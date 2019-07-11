25260ee4ad2bbe3bc736fb2dd97e1495eb701f8e
Paulo Montoya <paulo@paulomontoya.com.br>
2019-07-11 18:40:46 -0300

    Add favicon and title metatags

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handler = routes.getRequestHandler(app);
 
+const favicon = require("serve-favicon");
+
 app.prepare().then(() => {
   express()
+    .use(favicon(__dirname + "/static/favicon.ico"))
     .use(handler)
     .listen(3000);
 });
